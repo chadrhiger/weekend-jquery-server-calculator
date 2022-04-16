55804d971c25547fde4519d77d770effdd760c19
Chad Rhiger <csrhig@gmail.com>
2022-04-15 22:00:38 -0500

    initial commit.

---
+++ public/client.js
@@ -0,0 +1,7 @@
+console.log('js is happening');
+
+$(document).ready(onReady);
+
+function onReady(){
+  console.log('jq represent');
+}
---
+++ server.js
@@ -0,0 +1,5 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('bodyParser');
+const PORT = 5000;
+console.log('server is live in 3005');
